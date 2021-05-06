2357bf3c2f08a3be0a3194a34f347b23c5f1d545
Gabriel Paiva <gabrielrpaiva@gmail.com>
2021-05-06 09:42:59 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
-import 'express-async-errors';
+import cors from "cors";
+import "express-async-errors";
 import routes from "./routes";
 import uploadConfig from "./config/upload";
 
@@ -9,6 +10,7 @@ import AppError from "./errors/AppError";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
@@ -23,11 +25,10 @@ app.use(
     }
     console.error(err);
     return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-    })
-
-  },
+      status: "error",
+      message: "Internal server error",
+    });
+  }
 );
 
 app.listen(3333, () => {
